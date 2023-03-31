27f252ee1d402878fce77f81434eb996cb1aed68
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-03-31 12:46:30 +0900

    좌표 클래스

---
+++ src/main/java/coordinate/Coordinate.java
@@ -0,0 +1,11 @@
+package coordinate;
+
+public class Coordinate {
+    private int x;
+    private int y;
+
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
